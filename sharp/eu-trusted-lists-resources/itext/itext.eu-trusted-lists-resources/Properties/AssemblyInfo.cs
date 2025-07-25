508e0ce8c5674160aa1f504e9ec652a2d8ff8327
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2025-07-25 15:10:30 +0300

    Add AssemblyInfo.cs

---
+++ sharp/eu-trusted-lists-resources/itext/itext.eu-trusted-lists-resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("iText.EuTrustedListsResources")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Apryse Group NV")]
+[assembly: AssemblyProduct("iText")]
+[assembly: AssemblyCopyright("Copyright (c) 1998-2025 Apryse Group NV")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fae04ec0-301f-11d3-bf4b-00c04f79efbc")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
